ee548714e2e1b754be1d1dfd38cf50e797d23438
wolfoxstudy <wolfoxonly@qq.com>
2018-05-11 20:43:15 +0800

    uints

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -40,9 +40,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("QTUM");
-    case mBTC: return QString("mQTUM");
-    case uBTC: return QString::fromUtf8("μQTUM");
+    case BTC: return QString("PDX");
+    case mBTC: return QString("mPDX");
+    case uBTC: return QString::fromUtf8("μPDX");
     default: return QString("???");
     }
 }
@@ -51,9 +51,9 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("Qtums");
-    case mBTC: return QString("Milli-Qtums (1 / 1" THIN_SP_UTF8 "000)");
-    case uBTC: return QString("Micro-Qtums (1 / 1" THIN_SP_UTF8 "000" THIN_SP_UTF8 "000)");
+    case BTC: return QString("Predixs");
+    case mBTC: return QString("Milli-Predixs (1 / 1" THIN_SP_UTF8 "000)");
+    case uBTC: return QString("Micro-Predixs (1 / 1" THIN_SP_UTF8 "000" THIN_SP_UTF8 "000)");
     default: return QString("???");
     }
 }
